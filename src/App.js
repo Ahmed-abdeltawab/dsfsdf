42e27a7d9ce85f49ae8a36c9236cd30b4045c48a
Ahmed Abdel Tawab Mabrouk Moussa <78687842+Ahmed-abdeltawab@users.noreply.github.com>
2022-10-22 11:46:14 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox to vs code </h1>
+      <h1>aa 30`1022 </h1>
+    </div>
+  );
+}
